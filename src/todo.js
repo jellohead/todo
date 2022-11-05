b0111392b1ac5f2de471a84731f5872207c99b81
Pat Escalona <pat.escalona7@gmail.com>
2022-11-05 00:14:21 -0500

    Initial commit

---
+++ src/todo.js
@@ -0,0 +1,21 @@
+// todo.js
+
+
+
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    console.log('DOM loaded and parsed');
+
+})
+
+// Goals for this project
+// Properties: title, description, dueDate, priority, notes, checklist
+// Should have default list and should be able to create new lists like projects
+// First opening should produce a default todo list that everything goes into
+// After that user should be able to create new lists to group items
+// Look of UI:
+//     view all projects
+//     view all todos in each projects
+//     expand a single todo to see / edit details
+// delete a todo
+// Store data in localStorage
