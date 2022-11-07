ec5e1455a57141f03f74f9ed5ed3a7e0baa060e3
pat <pat.escalona7@gmail.com>
2022-11-07 08:00:38 -0600

    Handle adding new list item works but writes to the same div instead of creating a new line

--- src/todo.js
+++ src/todo.js
@@ -4,6 +4,24 @@ let storage;
 function addItemHandler() {
     console.log('in addItemHandler');
 
+    //
+    const listRow = document.querySelector('.container');
+    console.log(listRow);
+
+    let el = document.createElement('div')
+    el.setAttribute('class', 'list-item');
+    listRow.appendChild(el);
+
+    const listItem = document.querySelector('.list-item')
+
+    el = document.createElement('input')
+    el.setAttribute('type', 'checkbox');
+    // el.setAttribute('value', 'completed');
+    listItem.appendChild(el);
+
+    let newEl = document.createElement('input');
+    newEl.setAttribute('type', 'text');
+    listItem.appendChild(newEl);
 
 }
 
@@ -13,25 +31,24 @@ function addItemHandler() {
 window.addEventListener('DOMContentLoaded', (e) => {
     console.log('DOM loaded and parsed');
 
+    // add plus button and listener to DOM
     const headerOne = document.querySelector('h1');
     let el = document.createElement('div');
-    el.setAttribute('class', 'container');
+    el.setAttribute('class', 'plus-btn');
     headerOne.appendChild(el);
 
-    const plusDiv = document.querySelector('.container');
-    console.log(plusDiv);
+    const plusDiv = document.querySelector('.plus-btn');
     el = document.createElement('input');
     el.setAttribute('type', 'button');
     el.setAttribute('value', '+');
     el.setAttribute('id', 'add-item-button')
-    console.log(el);
     plusDiv.appendChild(el);
-
     el.addEventListener('click', addItemHandler, true);
 
-
-
-
+    // add container for todo list items
+    el = document.createElement('div');
+    el.setAttribute('class', 'container');
+    headerOne.after(el);
 })
 
 // Goals for this project
